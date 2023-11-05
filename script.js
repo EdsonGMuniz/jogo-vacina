a912819f84bb33acff1160bf57d9406ce0917fd4
edsonGmuniz <edsonmunizz@hotmail.com>
2023-11-05 18:11:34 -0300

    vacina

---
+++ script.js
@@ -0,0 +1,35 @@
+const container = document.querySelector(".container");
+const startBtn = document.querySelector(".startBtn");
+
+
+const terrorist = document.createElement("img");
+terrorist.setAttribute("class", "terrorist");
+terrorist.setAttribute("src", "bolso.png");
+
+
+const contHeight = container.offsetHeight;
+const contWidth = container.offsetWidth;
+
+setInterval(() => {
+  const randTop = Math.random() * (contHeight - 100);
+  const randLeft = Math.random() * (contWidth - 100);
+
+  terrorist.style.position = "absolute";
+  terrorist.style.top = randTop + "px";
+  terrorist.style.left = randLeft + "px";
+}, 1000);
+
+let score = 0;
+
+startBtn.addEventListener("click", () => {
+  container.appendChild(terrorist);
+
+  startBtn.innerText = "SCORE: " + score;
+});
+
+
+const cursor = document.querySelector(".cursor");
+window.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+});
